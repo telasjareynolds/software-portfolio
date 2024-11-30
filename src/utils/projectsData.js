90175ebb512bd3c1822071e1069c1acce0544a1a
Telasja Reynolds <telasjareynolds@gmail.com>
2024-11-30 15:42:36 -0500

    fix: image routing

--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -4,7 +4,7 @@ export const projects = [
     title: "Weather-Based Outfit Recommendation App",
     description:
       "Developed using a responsive web application using JavaScript, HTML, CSS, React.js, Node.js, MongoDB, and Express.js that dynamically recommends outfits based on real-time weather conditions. Leveraged a weather API to fetch accurate data and designed a user-friendly interface to enhance user engagement. The project showcases proficiency in RESTful API integration, state management, and responsive web design principles, while delivering practical functionality that demonstrates creative problem-solving in software development.",
-    image: "../../src/assets/project_wtwr.png",
+    image: "/software-portfolio/src/assets/project_wtwr.png",
     link: "https://www.projectreact.twilightparadox.com/",
     repo: "https://github.com/telasjareynolds/se_project_react",
   },
@@ -13,7 +13,7 @@ export const projects = [
     title: "Around The U.S. App",
     description:
       "Project built for TripleTen. It's a customizable profile for sharing memories, recipes, or travel plans. The project focuses on user-friendliness and customization, allowing easy addition, deletion, and editing of content. JavaScript validation ensures data integrity, and Object-oriented programming's reusable code classes streamline development. Challenges included designing and maintaining models, but troubleshooting and utilizing tools like console logs led to successful problem-solving. Future improvements may include adding a search bar for easier navigation.",
-    image: "../../src/assets/project_aroundtheus.png",
+    image: "/software-portfolio/src/assets/project_aroundtheus.png",
     link: "https://telasjareynolds.github.io/se_project_aroundtheus/",
     repo: "https://github.com/telasjareynolds/se_project_aroundtheus",
   },
@@ -22,7 +22,7 @@ export const projects = [
     title: "Fall Movie Night App",
     description:
       "I collaborated with a talented team to develop a fall-themed website that delivers a full movie night experience with just one click. My primary role was creating the UI, ensuring a cozy and immersive experience for users with elements like glowing jack-o'-lanterns, and to enhance the atmosphere, I used AI to generate the movie night background, adding a creative, dynamic visual. This project was a great opportunity to strengthen my front-end development while working closely with teammates to deliver a seamless and engaging user experience.",
-    image: "../../src/assets/project_movienight.jpg",
+    image: "/software-portfolio/src/assets/project_movienight.jpg",
     link: "https://cole-essig.github.io/TripTen-jam-squad/",
     repo: "https://github.com/telasjareynolds/TripTen-jam-squad",
   },
@@ -31,7 +31,7 @@ export const projects = [
     title: "Library App",
     description:
       "The Library App is a beautifully designed, user-friendly platform created to enhance the library experience. Built using HTML, CSS, and Flexbox, and designed according to a detailed Figma prototype, the app features sections for upcoming events, reader’s favorite book picks, and steps to become a library member. It also includes an informative section about the library, showcasing its services and history. This project demonstrates my ability to transform designs into responsive, visually appealing websites that prioritize user engagement and accessibility.",
-    image: "../../src/assets/project_library.png",
+    image: "/software-portfolio/src/assets/project_library.png",
     link: "https://telasjareynolds.github.io/se_project_library/",
     repo: "https://github.com/telasjareynolds/se_project_library",
   },
@@ -40,7 +40,7 @@ export const projects = [
     title: "Coffee Shop App",
     description:
       "The Coffee Shop App is a visually engaging, interactive website designed to bring the coffee shop experience online. Built with HTML, CSS, and Flexbox, and styled using the BEM methodology, the app features sections for YouTube video recipes, a table booking form, a detailed menu, and information about the coffee shop. Designed based on a Figma prototype, it incorporates smooth CSS animations to enhance user engagement. This project highlights my skills in creating responsive, accessible, and aesthetically pleasing websites that combine functionality with creativity.",
-    image: "../../src/assets/project_coffeeshop.png",
+    image: "/software-portfolio/src/assets/project_coffeeshop.png",
     link: "https://telasjareynolds.github.io/se_project_coffeeshop/",
     repo: "https://github.com/telasjareynolds/se_project_coffeeshop",
   },
@@ -49,7 +49,7 @@ export const projects = [
     title: "Project About Me",
     description:
       "This is an introductory project for the Software Engineering program at TripleTen. It was created using HTML and CSS, according to a design brief. It contains cards illustrating essential components that make up who I am. I have a great appreciation for movies, travel, reading, coding, and most importantly, family!",
-    image: "../../src/assets/project_aboutme.png",
+    image: "/software-portfolio/src/assets/project_aboutme.png",
     link: "https://telasjareynolds.github.io/project-0/",
   },
 ];
