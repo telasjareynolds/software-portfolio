011c9a04be2505ce284c586b8f054156d92afd12
Telasja Reynolds <telasjareynolds@gmail.com>
2024-11-30 02:18:14 -0500

    add deploy to package

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "software-portfolio",
+  base: "/software-portfolio/",
   server: {
     port: 3000,
   },
