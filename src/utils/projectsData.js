58d8dacc5840ac353929a099a8734e40d16680bc
Telasja Reynolds <telasjareynolds@gmail.com>
2025-01-30 09:36:47 -0500

    added project first connections

--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -5,28 +5,20 @@ import libraryImage from "../assets/project_library.png";
 import coffeShopImage from "../assets/project_coffeeshop.png";
 import aboutMeImage from "../assets/project_aboutme.png";
 import movieMazeImage from "../assets/project_movie-maze.png";
+import firstConnectionsImage from "../assets/project_firstConnections.jpg";
 
 export const projects = [
   {
     id: 1,
-    title: "Weather-Based Outfit Recommendation App",
+    title: "Project First Connections",
     description:
-      "Developed using a responsive web application using JavaScript, HTML, CSS, React.js, Node.js, MongoDB, and Express.js that dynamically recommends outfits based on real-time weather conditions. Leveraged a weather API to fetch accurate data and designed a user-friendly interface to enhance user engagement. The project showcases proficiency in RESTful API integration, state management, and responsive web design principles, while delivering practical functionality that demonstrates creative problem-solving in software development.",
-    image: wtwrImage,
-    link: "https://www.projectreact.twilightparadox.com/",
-    repo: "https://github.com/telasjareynolds/se_project_react",
+      "Volunteer project built a React-based website for First Connections VA, a nonprofit dedicated to improving early childhood care and education. The platform makes it easier for families, childcare professionals, and community partners to access essential resources, from training opportunities to food security assistance. Designed with user-friendly navigation, the site ensures seamless access to information, while search functionality helps users quickly find the support they need. Interactive maps allow families to locate food assistance programs, and dedicated pages provide tailored content for parents, professionals, and partners. This project reflects First Connections VA’s mission to empower communities and create brighter futures for children by making vital resources more accessible.",
+    image: firstConnectionsImage,
+    link: "https://telasjareynolds.github.io/project_first_connection/",
+    repo: "https://github.com/telasjareynolds/project_first_connection",
   },
   {
     id: 2,
-    title: "Around The U.S. App",
-    description:
-      "Project built for TripleTen. It's a customizable profile for sharing memories, recipes, or travel plans. The project focuses on user-friendliness and customization, allowing easy addition, deletion, and editing of content. JavaScript validation ensures data integrity, and Object-oriented programming's reusable code classes streamline development. Challenges included designing and maintaining models, but troubleshooting and utilizing tools like console logs led to successful problem-solving. Future improvements may include adding a search bar for easier navigation.",
-    image: aroundImage,
-    link: "https://telasjareynolds.github.io/se_project_aroundtheus/",
-    repo: "https://github.com/telasjareynolds/se_project_aroundtheus",
-  },
-  {
-    id: 3,
     title: "Movie Maze App",
     description:
       "I created Movie Maze to help movie enthusiasts discover and keep track of the world of cinema. Built using the OMDB third-party API and an in-progress custom REST API, it offers features like a curated home page of fan-favorite films, a personal watchlist for saving and managing movies, and detailed movie pages with comprehensive information about cast, crew, and storylines. Users can easily log in to sync their watchlist across devices and securely manage their accounts, while a robust search bar ensures they can quickly find any movie they’re looking for. As a software developer with a background in film production, I combined my passion for storytelling with technical expertise to create a seamless and engaging user experience for all movie lovers.",
@@ -35,7 +27,25 @@ export const projects = [
     repo: "https://github.com/telasjareynolds/movie-maze-frontend",
   },
   {
+    id: 3,
+    title: "Weather-Based Outfit Recommendation App",
+    description:
+      "Developed using a responsive web application using JavaScript, HTML, CSS, React.js, Node.js, MongoDB, and Express.js that dynamically recommends outfits based on real-time weather conditions. Leveraged a weather API to fetch accurate data and designed a user-friendly interface to enhance user engagement. The project showcases proficiency in RESTful API integration, state management, and responsive web design principles, while delivering practical functionality that demonstrates creative problem-solving in software development.",
+    image: wtwrImage,
+    link: "https://www.projectreact.twilightparadox.com/",
+    repo: "https://github.com/telasjareynolds/se_project_react",
+  },
+  {
     id: 4,
+    title: "Around The U.S. App",
+    description:
+      "Project built for TripleTen. It's a customizable profile for sharing memories, recipes, or travel plans. The project focuses on user-friendliness and customization, allowing easy addition, deletion, and editing of content. JavaScript validation ensures data integrity, and Object-oriented programming's reusable code classes streamline development. Challenges included designing and maintaining models, but troubleshooting and utilizing tools like console logs led to successful problem-solving. Future improvements may include adding a search bar for easier navigation.",
+    image: aroundImage,
+    link: "https://telasjareynolds.github.io/se_project_aroundtheus/",
+    repo: "https://github.com/telasjareynolds/se_project_aroundtheus",
+  },
+  {
+    id: 5,
     title: "Fall Movie Night App",
     description:
       "I collaborated with a talented team to develop a fall-themed website that delivers a full movie night experience with just one click. My primary role was creating the UI, ensuring a cozy and immersive experience for users with elements like glowing jack-o'-lanterns, and to enhance the atmosphere, I used AI to generate the movie night background, adding a creative, dynamic visual. This project was a great opportunity to strengthen my front-end development while working closely with teammates to deliver a seamless and engaging user experience.",
@@ -44,7 +54,7 @@ export const projects = [
     repo: "https://github.com/telasjareynolds/TripTen-jam-squad",
   },
   {
-    id: 5,
+    id: 6,
     title: "Library App",
     description:
       "The Library App is a beautifully designed, user-friendly platform created to enhance the library experience. Built using HTML, CSS, and Flexbox, and designed according to a detailed Figma prototype, the app features sections for upcoming events, reader’s favorite book picks, and steps to become a library member. It also includes an informative section about the library, showcasing its services and history. This project demonstrates my ability to transform designs into responsive, visually appealing websites that prioritize user engagement and accessibility.",
@@ -53,7 +63,7 @@ export const projects = [
     repo: "https://github.com/telasjareynolds/se_project_library",
   },
   {
-    id: 6,
+    id: 7,
     title: "Coffee Shop App",
     description:
       "The Coffee Shop App is a visually engaging, interactive website designed to bring the coffee shop experience online. Built with HTML, CSS, and Flexbox, and styled using the BEM methodology, the app features sections for YouTube video recipes, a table booking form, a detailed menu, and information about the coffee shop. Designed based on a Figma prototype, it incorporates smooth CSS animations to enhance user engagement. This project highlights my skills in creating responsive, accessible, and aesthetically pleasing websites that combine functionality with creativity.",
@@ -62,7 +72,7 @@ export const projects = [
     repo: "https://github.com/telasjareynolds/se_project_coffeeshop",
   },
   {
-    id: 7,
+    id: 8,
     title: "Project About Me",
     description:
       "This is an introductory project for the Software Engineering program at TripleTen. It was created using HTML and CSS, according to a design brief. It contains cards illustrating essential components that make up who I am. I have a great appreciation for movies, travel, reading, coding, and most importantly, family!",
